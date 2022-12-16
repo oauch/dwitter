7262ac77ba5598f72a43dbfb416de5561ce0638c
oauch <lgm9515@naver.com>
2022-12-16 13:25:29 +0900

    Updates

---
+++ precache-manifest.4eff1e6dd671138b42183ce7ab430251.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a76bddfe40c915cbde9",
+    "url": "/static/css/main.28dc3693.chunk.css"
+  },
+  {
+    "revision": "5a76bddfe40c915cbde9",
+    "url": "/static/js/main.cec584db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ad9628b446d8b964ee9",
+    "url": "/static/js/2.042650c6.chunk.js"
+  },
+  {
+    "revision": "c7fc92b12948e4622c92a4dbd5649af8",
+    "url": "/index.html"
+  }
+];
